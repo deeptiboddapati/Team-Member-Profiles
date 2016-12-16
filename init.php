19bc759c604d5673601f66f6aca84c1c825f55dd
deeptiboddapati <deeptiboddapati@gmail.com>
2016-12-15 20:50:50 -0600

    wp query, alphabatized, and 12 posts

--- init.php
+++ init.php
@@ -11,13 +11,14 @@ Author: Deepti Boddapati
 Version: 1.0
 Author URI: http://www.deeptiboddapati.com
 */
-// add_action( 'admin_enqueue_scripts', 'DB_staff_admin_scripts_and_styles', 999 );
+
+add_action( 'admin_enqueue_scripts', 'DB_staff_admin_scripts_and_styles', 999 );
 function DB_staff_admin_scripts_and_styles(){
 
- wp_enqueue_style(
+   wp_enqueue_style(
     'mod-admin',
-   plugin_dir_url( __FILE__ ) . 'style.css'
-  );
+    plugin_dir_url( __FILE__ ) . 'style.css'
+    );
 
 
    wp_enqueue_script(
@@ -26,46 +27,48 @@ function DB_staff_admin_scripts_and_styles(){
     ['jquery'],
     '1.15.1',
     'true'
-  );
+    );
 
-    wp_enqueue_script(
+   wp_enqueue_script(
     'mod-admin',
     plugin_dir_url( __FILE__ ) . 'admin.js',
     ['jquery-validate'],
     null,
     'true'
-  );
+    );
 
 
 }
 add_action( 'init', 'staffcpt' );
 function staffcpt(){
-	$args = array(
-	'labels' => array('name' => 'Team Member'),
-	'supports' => array( 'title', 'editor', 'thumbnail' ),
-	'show_ui'  => true,
-	'show_in_menu' => true,
-	'query_var'          => true,
-	'public' => true
-);
-register_post_type('staff', $args);
+    $labels= array('name' => 'Team Member');
+    $args = array(
+       'labels' => $labels,
+       'supports' => array( 'title', 'editor', 'thumbnail' ),
+       'show_ui'  => true,
+       'show_in_menu' => true,
+       'query_var'          => true,
+       'public' => true
+       );
 
-$labels = array(
-	'name'                       => 'Department'
-);
+    register_post_type('staff', $args);
 
-$args = array(
-	'hierarchical'          => false,
-	'labels'                => $labels,
-	'show_ui'               => true,
-	'show_admin_column'     => true,
-	'update_count_callback' => '_update_post_term_count',
-	'query_var'             => true,
-	'rewrite'               => array( 'slug' => 'department' ),
-);
+    $labels = array(
+       'name'                       => 'Department'
+       );
+
+    $args = array(
+       'hierarchical'          => false,
+       'labels'                => $labels,
+       'show_ui'               => true,
+       'show_admin_column'     => true,
+       'update_count_callback' => '_update_post_term_count',
+       'query_var'             => true,
+       'rewrite'               => array( 'slug' => 'department' ),
+       );
 
 
-register_taxonomy('department','staff',$args);
+    register_taxonomy('department','staff',$args);
 }
 add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
 /**
@@ -88,49 +91,46 @@ function cmb2_sample_metaboxes() {
         'show_names'    => true, // Show field names on the left
         // 'cmb_styles' => false, // false to disable the CMB stylesheet
         // 'closed'     => true, // Keep the metabox closed by default
-    ) );
+        ) );
 
-    // Regular text field
+   // Regular text field
     $cmb->add_field( array(
-        'name'       => __( 'Test Text', 'cmb2' ),
-        'desc'       => __( 'field description (optional)', 'cmb2' ),
-        'id'         => $prefix . 'text',
+        'name'       => __( 'Position', 'cmb2' ),    
+        'id'         => $prefix . 'position',
         'type'       => 'text',
         'sanitization_cb' => 'sanitize_text_field', // custom sanitization callback parameter
-        // 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
+        'escape_cb'       => 'sanitize_text_field',  // custom escaping callback parameter
         // 'on_front'        => false, // Optionally designate a field to wp-admin only
         // 'repeatable'      => true,
-    ) );
+        ) );
 
     // URL text field
-    //escape_cb- cmb automatically passes entries in a url field to esc_url
+    //escape_cb- cmb automatically passes entries in a url field to esc_url this is sufficent for displaying urls
     //sanitization cb- cmb automatically passes entries to esc url if no other sanitization cb is added
     $cmb->add_field( array(
-        'name' => __( 'Website URL', 'cmb2' ),
-        'desc' => __( 'field description (optional)', 'cmb2' ),
+        'name' => __( 'Twitter Profile URL', 'cmb2' ),
         'id'   => $prefix . 'twitter_url',
         'type' => 'text_url',
         'protocols' => array('http', 'https'), // Array of allowed protocols
         'repeatable' => false,
         'sanitization_cb' => 'DB_sanitize_social_media_url'
-    ) );
+        ) );
         // URL text field
     $cmb->add_field( array(
-        'name' => __( 'Website URL', 'cmb2' ),
-        'desc' => __( 'field description (optional)', 'cmb2' ),
+        'name' => __( 'FaceBook Profile URL', 'cmb2' ),
         'id'   => $prefix . 'facebook_url',
         'type' => 'text_url',
         'protocols' => array('http', 'https'), // Array of allowed protocols
         'repeatable' => false,
         'sanitization_cb' => 'DB_sanitize_social_media_url'
 
-    ) );
+        ) );
     
 
 }
 
 function DB_sanitize_social_media_url($value, $field_args, $field){
-    
+
     if($field_args['id'] == '_DB_stafftwitter_url'){
         if(!preg_match("/twitter.com/i", $value)){
             return '';
@@ -138,12 +138,11 @@ function DB_sanitize_social_media_url($value, $field_args, $field){
     }
     else{
         if(!preg_match("/facebook.com/i", $value)){
-        return '';
+            return '';
+        }
     }
-    }
-   
-    return esc_url($value, array('http', 'https'));
-    
+
+    return esc_url($value, array('http', 'https'));  
 }
 
 // add_filter( 'clean_url', 'DB_checking_esc_url',10,  3 );
@@ -156,4 +155,27 @@ function DB_checking_esc_url($good_protocol_url, $original_url, $_context){
 
 }
 
- ?>
+
+// add_filter( 'esc_html', 'DB_check_esc_html_ran',10,  2 );
+function DB_check_esc_html_ran( $safe_text, $text){
+    error_log(print_r(__FUNCTION__.$text,true));
+    return $safe_text;
+}
+
+// add_filter( 'sanitize_text_field', 'DB_check_sanitize_text_field_ran',10,  2 );
+function DB_check_sanitize_text_field_ran( $safe_text, $text){
+    error_log(print_r(__FUNCTION__.$text,true));
+    return $safe_text;
+}
+
+function staff_query(){
+     $args = array(
+        'post_type' => 'staff', 
+        'orderby' => 'title', 
+        'order' => 'DESC',
+        'posts_per_page' =>12
+        );
+     $query = new $WP_query($args);
+}
+
+?>
