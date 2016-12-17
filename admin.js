94e35111ba6cdf1bfd611cf0f09e433505336ba5
deeptiboddapati <deeptiboddapati@gmail.com>
2016-12-17 17:19:34 -0600

    added js behavior for sliding the team member bio in and out

--- admin.js
+++ admin.js
@@ -9,10 +9,25 @@
     $(this).closest('form').validate();
 
     $(this).find('.cmb2-text-url').each(function() {
-      console.log(this);
       $(this).rules( "add", {
         url: true,
       });
     });
+  });
+
+  $('.readmore').click(function(){
+    $(this).slideUp();
+    $(this).next('.description').slideDown('slow', function(){
+    });
+    // $(this).next('.description').addClass('active');
+  });
+  $(document).ready(function(){
+    $('.description').slideUp();
+  })
+
+  $('.readless').click(function(){
+    $(this).parent().slideUp();
+    console.log($(this).parent().prev('.readmore'));
+    $(this).parent().prev('.readmore').slideDown();
   });
 })(jQuery);
